8c3be467f448f12a1c6aad12b1094aa3bec99573
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-10 12:48:16 -0600

    refactor: 💡 poniendo las credenciales para la base de daos

--- backend/factura_farmacia/factura_farmacia/settings.py
+++ backend/factura_farmacia/factura_farmacia/settings.py
@@ -76,17 +76,16 @@ WSGI_APPLICATION = 'factura_farmacia.wsgi.application'
 
 DATABASES = {
     'default': {
-         'default': {
+
         'ENGINE': 'django.db.backends.mysql',
-        'HOST':'localhost',
-        'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':'Dragon97',
-        'NAME':'farmacia',
-        'OPTIONS':{
-            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Dragon97',
+        'NAME': 'factura_farmacia',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
-    }
     }
 }
 
