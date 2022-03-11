656a20dd9fb5324cfd92b22a8a7bbf107beb68e1
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-10 18:28:29 -0600

    refactor: 💡 vistas de los modelos de producto, factura etc

--- backend/factura_farmacia/apiFactura/admin.py
+++ backend/factura_farmacia/apiFactura/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Persona
+
+from .models import Persona, Factura, Categoria, Producto
 
 # Register your models here.
 admin.site.register(Persona)
+admin.site.register(Factura)
+admin.site.register(Categoria)
+admin.site.register(Producto)
