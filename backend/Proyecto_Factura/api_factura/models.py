1aaae33921ad5f18448259cd527a75bc997f8812
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-11 14:06:45 -0600

    feat: 🎸 nuevos modelos de la DB y conexion a ella

---
+++ backend/Proyecto_Factura/api_factura/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+# Create your models here.
+
+
+class Persona(models.Model):
+    MASCULINO = 'M'
+    FEMENINO = 'F'
+    GENERO = [(MASCULINO, 'Masculino'), (FEMENINO, 'Femenino')]
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=8)
+    genero = models.CharField(
+        max_length=10, choices=GENERO, default='Masculino')
+    numero_identidad = models.CharField(max_length=13)
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=30)
+
+
+class Producto(models.Model):
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    precio_costo = models.DecimalField(max_digits=6, decimal_places=2)
+    nombre_producto = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=30)
+    fecha_elaboracion = models.DateField(
+        auto_now_add=False, null=True, blank=True)
+    fecha_vencimiento = models.DateField(
+        auto_now_add=False, null=True, blank=True)
+
+    #  poner la categoria
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
+
+class Factura(models.Model):
+    fecha = models.DateField(auto_now=True)
+    total = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    numeroIdentidad = models.ForeignKey(Persona, on_delete=models.CASCADE)
+
+
+class Factura_detalle(models.Model):
+    productos_productos_id = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    factura_factura_id = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    cantidad = models.DecimalField(max_digits=3, decimal_places=2, null=True)
+
+
+
+#modelos de impuestos
+class Descripcion_impuestos_descuentos(models.Model):
+    descripcion = models.CharField(max_length=30)
+    valor = models.DecimalField(max_digits=2, decimal_places=2)
+    estado = models.BooleanField(default=True, null=False)
+
+
+
+
