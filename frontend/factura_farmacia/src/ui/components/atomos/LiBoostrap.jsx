ceaa18af6dd9a20001121d13e6b17b1131fba4c8
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-11 20:39:29 -0600

    refactor: 💡 falta terminar la navegacion

--- frontend/factura_farmacia/src/ui/components/atomos/LiBoostrap.jsx
+++ frontend/factura_farmacia/src/ui/components/atomos/LiBoostrap.jsx
@@ -1,3 +1,22 @@
+import { Link, NavLink } from "react-router-dom";
+
+
+// todo: terminar la navegacion
+
 export const LiBoostrap = ({ estiloClassName, titulo }) => {
-  return <li className={estiloClassName}>{titulo}</li>;
+  return (
+    <>
+      <nav>
+        {/* <NavLink
+        className={({ isActivate }) =>
+          "nav-item nav-link" + (isActivate ? "active" : "")
+        }
+        to="categoria"
+      >{titulo}</NavLink> */}
+
+
+        <li className={estiloClassName}>{titulo}</li> 
+      </nav>
+    </>
+  );
 };
