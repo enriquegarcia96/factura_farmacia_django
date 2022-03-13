341b8dc56ec13be23ee85baf3f8d1130f8c74901
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-12 22:26:51 -0600

    refactor: 💡 llamando al endpoint del backend

--- frontend/factura_farmacia/src/ui/components/pages/CategoriaScreen.jsx
+++ frontend/factura_farmacia/src/ui/components/pages/CategoriaScreen.jsx
@@ -1,7 +1,40 @@
-
+import { useEffect, useState } from "react";
+import { FormularioCategoriaScreen } from "../moleculas/FormularioCategoriaScreen";
+import { apiCategorias } from "./ApiFactura";
 
 export const CategoriaScreen = () => {
+  const [categoria, setCategoria] = useState([]);
+  const [descripcion2, setDescripcion] = useState();
+
+  const categorias = async () => {
+    try {
+      const res = await apiCategorias();
+      const { Categoria } = await res.json();
+      setCategoria(Categoria);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    categorias();
+  }, []);
+
+  //TODO: hacer la inserccion de categoria
+
+  const handleButton = (e) => {
+    e.preventDefault();
+    console.log(categoria);
+  };
+
   return (
-    <div>CategoriaScreen</div>
-  )
-}
+    <div>
+      <FormularioCategoriaScreen
+        nombreCategoria={"Descripcion"}
+        descripcion={"Descripcion"}
+        categoria={categoria}
+        event={handleButton}
+      />
+    </div>
+  );
+};
