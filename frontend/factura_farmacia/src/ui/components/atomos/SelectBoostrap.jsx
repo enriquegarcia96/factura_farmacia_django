56e4ed5b9c85e3006547a221581cb2d3ae01d1e6
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-12 20:05:03 -0600

    refactor: 💡 NavLink para la navegacion del menu

--- frontend/factura_farmacia/src/ui/components/atomos/SelectBoostrap.jsx
+++ frontend/factura_farmacia/src/ui/components/atomos/SelectBoostrap.jsx
@@ -1,10 +1,8 @@
-
-
 export const SelectBoostrap = () => {
-    return (
-        <select className="form-select" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-        </select>
-    )
-}
+  return (
+    <select className="form-select" aria-label="Default select example">
+      <option selected>Open this select menu</option>
+      <option value="1">One</option>
+    </select>
+  );
+};
