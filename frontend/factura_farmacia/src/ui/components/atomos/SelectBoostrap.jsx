c0d1e989f37dbf630c72624f7c12b18ba916f8e6
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-14 09:00:48 -0600

    refactor: 💡 colocando la key, onchange y el value a las option

--- frontend/factura_farmacia/src/ui/components/atomos/SelectBoostrap.jsx
+++ frontend/factura_farmacia/src/ui/components/atomos/SelectBoostrap.jsx
@@ -1,9 +1,18 @@
-export const SelectBoostrap = ({ categoria = [] }) => {
-  console.log(categoria);
+import { useState } from "react";
+
+export const SelectBoostrap = ({ categoria = [], submit }) => {
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("");
+
+  submit(categoriaSeleccionada);
+
   return (
     <select className="form-select" aria-label="Default select example">
       {categoria.map((categoria) => (
-        <option key={categoria.id} value={categoria.id}>
+        <option
+          key={categoria.id}
+          onChange={setCategoriaSeleccionada}
+          value={categoriaSeleccionada}
+        >
           {categoria.nombre}
         </option>
       ))}
