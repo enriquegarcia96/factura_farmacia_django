dc060fd45ac181ecf4c38b22b26f436757653858
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-14 09:16:13 -0600

    refactor: 💡 vista de facturacion

--- frontend/factura_farmacia/src/ui/components/pages/FacturaVentaScreen.jsx
+++ frontend/factura_farmacia/src/ui/components/pages/FacturaVentaScreen.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { FormularioFactura } from "../moleculas/FormularioFactura";
+import { apiProductos } from "./ApiFactura";
 
 export const FacturaVentaScreen = () => {
-  return (
-    <div>FacturaVentaScreen</div>
-  )
-}
+  const [productos, setProductos] = useState([]);
+  
+  const productosApi = async () => {
+    try {
+      const resp = await apiProductos();
+      const { Productos } = await resp.json();
+      setProductos(Productos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  useEffect(() => {
+    productosApi();
+  }, [setProductos]);
+
+  return <FormularioFactura productos={productos} />;
+};
