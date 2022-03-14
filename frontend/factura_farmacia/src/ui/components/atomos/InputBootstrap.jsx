4d2d386b92b1ddaed4730dce79987f8984ec2d8c
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-14 08:59:59 -0600

    refactor: 💡 colocando el value y el disable al input

--- frontend/factura_farmacia/src/ui/components/atomos/InputBootstrap.jsx
+++ frontend/factura_farmacia/src/ui/components/atomos/InputBootstrap.jsx
@@ -1,11 +1,22 @@
-
-
-
-export const InputBoostrap = ({ tituloSpan, estiloClassName, placeholder, type }) => {
+export const InputBoostrap = ({
+  tituloSpan,
+  estiloClassName,
+  placeholder,
+  type,
+  value = 0,
+  disabled = false
+}) => {
   return (
     <div className="input-group">
       <span className="input-group-text">{tituloSpan}</span>
-      <input type={type} aria-label="First name" className={estiloClassName} placeholder={placeholder}  />
+      <input
+        type={type}
+        aria-label="First name"
+        className={estiloClassName}
+        placeholder={placeholder}
+        value={value}
+        disabled={disabled}
+      />
     </div>
-  )
-}
+  );
+};
